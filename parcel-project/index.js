0f99860ba524049f2c80fcbcfc18f29039e6bed8
Brownbull <carcamo.gabriel@gmail.com>
2018-08-17 07:55:43 -0300

    Parcel project done

---
+++ parcel-project/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = () => {
+  return (
+    <div>
+      Whatsup!?
+    </div>
+  )
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
